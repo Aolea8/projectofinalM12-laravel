66f055c478e2b516e93779e5f43fb53165e83e9a
Olea <arnauolea@gmail.com>
2023-05-30 17:32:50 +0200

    cambios controller videos

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TokenController;
 use App\Http\Controllers\Api\FavoriteController;
 use App\Http\Controllers\Api\CommentController;
-use App\Http\Controllers\Api\PeliController;
+use App\Http\Controllers\Api\PelisController;
 
 
 /*
@@ -29,9 +29,9 @@ Route::get('/user/favorites', [FavoriteController::class, 'index'])->middleware(
 Route::get('/peliserie/{idpeliserie}/comments', [CommentController::class, 'comments']);
 Route::post('/peliserie/{idpeliserie}/comment', [CommentController::class, 'comment'])->middleware(['auth:sanctum']);
 Route::delete('/peliserie/uncomment/{comment}', [CommentController::class, 'uncomment'])->middleware(['auth:sanctum']);
-Route::post('/videos', [PeliController::class, 'store'])->middleware(['auth:sanctum']);
-Route::put('/videos/{peli}', [PeliController::class, 'update'])->middleware(['auth:sanctum']);
-Route::delete('/videos/{peli}', [PeliController::class, 'destroy'])->middleware(['auth:sanctum']);
-Route::get('/videos', [PeliController::class, 'index'])->middleware(['auth:sanctum']);
-Route::get('/videos/{peli}', [PeliController::class, 'show'])->middleware(['auth:sanctum']);
+Route::post('/videos', [PelisController::class, 'store'])->middleware(['auth:sanctum']);
+Route::put('/videos/{peli}', [PelisController::class, 'update'])->middleware(['auth:sanctum']);
+Route::delete('/videos/{peli}', [PelisController::class, 'destroy'])->middleware(['auth:sanctum']);
+Route::get('/videos', [PelisController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/videos/{peli}', [PelisController::class, 'show'])->middleware(['auth:sanctum']);
 
